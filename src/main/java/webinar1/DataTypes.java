17fb60f73d8990716c6f51e38cced8600d155a69
MedSveta <sveta1978medved@gmail.com>
2025-07-30 19:20:35 +0300

    Webinar1

--- src/main/java/webinar1/DataTypes.java
+++ src/main/java/webinar1/DataTypes.java
@@ -1,4 +1,68 @@
 package webinar1;
 
 public class DataTypes {
+    static int b;
+    static double b1;
+    static boolean b2;
+    static char b3;
+
+
+    public static void main(String[] args) {
+        //примитивные
+        //целочисленные byte; short; int; long -9,223,372,036,854,775,808.    .9,223,372,036,854,775,807;
+        // с плавающей точкой double; float;
+        // символьные char;
+        // логические boolean;
+
+        int a;
+        //System.out.println(a);
+        System.out.println(b);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+
+        long l = 10_000_000_00000L;
+        float f = 3.0986675432f;
+
+        char char1 = 65;
+        System.out.println(char1);
+        char char2 = 'A';
+        System.out.println(char2);
+        System.out.println((int) char2);
+        for (int i = 65; i < 80; i++) {
+            char char3 = (char) i;
+            System.out.print(char3 + " : ");
+            System.out.println("//====================================");
+
+            String string1 = "qwerty";
+            String string2 = "qwerty";
+            if (string1 == string2)
+                System.out.println("strings value equal");
+            else
+                System.out.println("strings value not equal");
+            String string3 = new String("Zrty");
+            String string4 = new String("zrty");
+
+            if (string3 == string4)
+                System.out.println("strings value equal");
+            else
+                System.out.println("strings value not equal");
+
+            if (string3.equals(string4) == true)
+                System.out.println("strings value equal");
+            else
+                System.out.println("strings value not equal");
+
+            if (string3.equalsIgnoreCase(string4) == true)
+                System.out.println("strings value equal");
+            else
+                System.out.println("strings value not equal");
+
+
+        }
+    }
+
+    private String first(){
+        return null;
+    }
 }
--- src/main/java/webinar1/Wrappers.java
+++ src/main/java/webinar1/Wrappers.java
@@ -1,4 +1,52 @@
 package webinar1;
 
 public class Wrappers {
+    public static void main(String[] args) {
+      /*
+	 byte => Byte
+	 short => Short
+	 int => Integer
+	 long => Long
+	 boolean => Boolean
+	 char => Character
+	 float => Float
+	 double => Double
+	 */
+
+        int a = 12;
+        Integer b = 45;
+        String sb = b.toString();
+        System.out.println(sb + a);
+        int c = Integer.parseInt("345");
+        System.out.println(c + a);
+        //c = Integer.parseInt("345a34"); => Exception
+        System.out.println(c + a);
+
+        String f = "345.56";
+        float f1 = Float.parseFloat(f);
+        System.out.println(f1*10);
+
+        boolean bol = Boolean.parseBoolean("TruE");
+        System.out.println(bol);
+
+        System.out.println(Character.isDigit('4'));
+        System.out.println(Character.isDigit('w'));
+        System.out.println(Character.isDigit(65));
+        System.out.println(Character.isLowerCase('w'));
+        System.out.println(Character.isLowerCase('A'));
+        System.out.println(Character.isLetterOrDigit('@'));
+
+        Integer i1 = Integer.max(3, 6);
+        System.out.println(i1);
+        Integer i2 = Integer.min(1, 7);
+        System.out.println(i2);
+
+        int i3 = i2; //распаковка до примитива
+        Integer i4 = i3;
+
+        String[] str = new String[]{};
+        String[] str1 = {"str1", "str2", "str3", null};
+        String[] str2 = new String[3];
+        str2[0] = "tyui";
+    }
 }
